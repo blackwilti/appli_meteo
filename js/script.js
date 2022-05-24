1b04ea965d79f2939d665d20d2d176902a3821fa
wilti <william.titus.nsm@gmail.com>
2022-05-24 16:27:14 +0200

    version3

--- js/script.js
+++ js/script.js
@@ -1,42 +1,48 @@
+const weatherIcons = {
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-sunny",
+    "Snow": "wi wi-day-snow",
+    "Mist": "wi wi-day-fog",
+    "Drizzle": "wi wi-day-sleet",
+}
 
-const weatherIcons =  { 
-    Rain: "wi wi-day-rain",
-    Clouds: "wi wi-day-cloudy",
-    Clear: "wi wi-day-sunny",
-    Snow: "wi wi-day-snow",
-    mist: "wi wi-day-fog",
-    Drizzle: "wi wi-day-sleet",
-} 
-capitalize = (str) => {
+
+function capitalize(str){
     return str[0].toUpperCase() + str.slice(1);
 }
-
-
-main = async () => {
-    const ip = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_idJydwp2Va6gdXL4eAPAcpe176tOF&`)
-    .then(resultat => resultat.json())
-    .then(json =>  json.ip)
-
-    const ville = await fetch(`http://api.ipstack.com/31.35.20.150?access_key=e94778bf7ad8466e46bf02b4f4fd36e4`)
-    .then(resultat => resultat.json())
-    .then(json => document.querySelector('#ville').textContent = json.city)
-    
-    const meteo = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2f2bde283d34e657c028e1524df570b4&units=metric&lang=fr`)
-    .then(resultat => resultat.json())
-    .then(json => displayWeatherInfos(json))          
-}
-    
-    displayWeatherInfos = (data) => {
-    
-    const name  = data.city.name;
-    const temperature = data.list[data.list.length-1].main.temp;
-    const conditions = data.list[data.list.length-1].weather[data.list[data.list.length-1].weather.length-1].description;
+async function main(){
+  
+  const ip = await fetch("https://geo.ipify.org/api/v2/country?apiKey=at_idJydwp2Va6gdXL4eAPAcpe176tOF&format=json")
+    .then((resultat) => resultat.json())
+    .then((json) => json.ip);
  
-    document.querySelector('#temperature').textContent = Math.round(temperature);
-    document.querySelector('#conditions').textContent = capitalize(conditions);
-    if(document.querySelector('icon')){
-        document.querySelector('icon').className = weatherIcons[conditions];
-    }  
+  const ville = await fetch(
+    `http://api.ipstack.com/${ip}?access_key=d82c5e16dd0bf2ad8a5b189d4679eb0d`
+  )
+    .then((resultat) => resultat.json())
+    .then((json) => json.city);
+  
+  const meteo = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=675676a6815a678c46bb033c60a9c39f&lang=fr&units=metric`
+  )
+    .then((resultat) => resultat.json())
+    .then((json) => json);
+ 
+  displayWeatherInfos(meteo);
 }
+function displayWeatherInfos(data){
+    const name = data.name;
+    const temperature = data.main.temp;
+    const conditions = data.weather[0].main;
+    const description = data.weather[0].description;
+    document.querySelector('#ville').textContent = name;
+    document.querySelector('#temperature').textContent= Math.round(temperature);
+    document.querySelector('#conditions').textContent = capitalize(description);
+
+    document.querySelector("i.wi").className = weatherIcons[conditions];
+    document.body.className = conditions.toLowerCase();
+}
+
 main();
 
